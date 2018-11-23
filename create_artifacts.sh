8ce76635fb897c45e37d4a95099211ece47e6ed0
Lachlan Donald <lachlan@buildkite.com>
2018-11-23 11:59:00 +1100

    Bump

--- create_artifacts.sh
+++ create_artifacts.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -euo pipefail
 
-mkdir -p log/
-echo "hello world" > log/test.log
+#mkdir -p log/
+#echo "hello world" > log/test.log
 
 mkdir -p client-src/integration-tests/screenshots/llamas
 cp llama.png client-src/integration-tests/screenshots/llamas/
